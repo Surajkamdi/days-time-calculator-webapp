a578aa99d39630e1f23bb9211895e20f3cf61a78
Suraj Kamdi <>
2020-04-21 13:39:57 +0530

    Updates

---
+++ precache-manifest.ea64595a9150fd1a5a216e8e8c3c4c65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0de8a4f6e9bd9d5718203674105d26",
+    "url": "/days-calculator-reactjs/index.html"
+  },
+  {
+    "revision": "7491e37307215bcbb067",
+    "url": "/days-calculator-reactjs/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1b0be26f3608ef7f5cbc",
+    "url": "/days-calculator-reactjs/static/js/2.496f2f48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/days-calculator-reactjs/static/js/2.496f2f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7491e37307215bcbb067",
+    "url": "/days-calculator-reactjs/static/js/main.a6b40ea0.chunk.js"
+  },
+  {
+    "revision": "f626f5ddad1ae5c1bdc2",
+    "url": "/days-calculator-reactjs/static/js/runtime-main.7c6a7c2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/days-calculator-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
