8a591b438650cfbaf07eaf24180ecac2b9704d18
Suraj Kamdi <suraj.kamdi@perficient.com>
2020-04-23 16:54:54 +0530

    Updated Google Analytics tracking id.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+//import './index.css';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ReactGA from 'react-ga';
 
@@ -9,6 +9,7 @@ import ReactGA from 'react-ga';
  * Google Analytics Code.
  */
 ReactGA.initialize('G-5LR83M4MJ0');
+ReactGA.initialize('UA-164371994-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
